29ebe59b1079cb3c91a4bb45aeb7fa1e014c6828
Rishhi <98315242+rishhi-patel@users.noreply.github.com>
2025-08-14 16:30:13 -0400

    Update client.model.ts

--- microservices/client-service/src/models/client.model.ts
+++ microservices/client-service/src/models/client.model.ts
@@ -6,7 +6,6 @@ const clientSchema = new mongoose.Schema(
     email: {
       type: String,
       required: true,
-      unique: true,
       match: [/^\S+@\S+\.\S+$/, "Please provide a valid email address"],
     },
     phone: { type: String },
